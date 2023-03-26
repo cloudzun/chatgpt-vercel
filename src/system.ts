18926a9a86ff448d0753a84e76b57e10801b2f22
CloudZun <info@cloudzun.com>
2023-03-26 16:25:27 +0800

    Update system.ts

--- src/system.ts
+++ src/system.ts
@@ -10,8 +10,7 @@ export const defaultSetting = {
   model: "gpt-3.5-turbo" as Model
 }
 
-export const defaultMessage = `Powered by OpenAI Vercel
-- 本网站基于 [OpenAI API (gpt-3.5-turbo)](https://platform.openai.com/docs/guides/chat) 技术，仅用于演示。
+export const defaultMessage = `本网站基于 OpenAI (gpt-3.5-turbo) 技术，仅用于演示。
 - 由 [@CloudZun](https://github.com/cloudzun) 运维， [项目代码](https://github.com/cloudzun/chatgpt-vercel) 源自 [@ourongxing](https://github.com/ourongxing) 和 [@ddiu8081](https://github.com/ddiu8081/chatgpt-demo)。
 - [[Shift]] + [[Enter]] 换行。开头输入 [[/]] 或者 [[空格]] 搜索 Prompt 预设。[[↑]] 可编辑最近一次提问。点击顶部名称滚动到顶部，点击输入框滚动到底部。`
 
